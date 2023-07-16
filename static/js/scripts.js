f67bce58e2d2650a7f426cdc9718881dcbd4d7b0
JAndersonArruda <ferrazarrudaanderson@gmail.com>
2023-07-15 22:48:46 -0300

    update pendencias

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -6,3 +6,21 @@ document.addEventListener('DOMContentLoaded', function() {
         navSecondary.classList.toggle('show');
     });
 });
+
+document.getElementById("problemForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var description = document.getElementById("description").value;
+
+    // Aqui você pode fazer algo com os dados, como enviar para um servidor
+
+    // Exemplo de exibição dos dados no console
+    console.log("Nome: " + name);
+    console.log("E-mail: " + email);
+    console.log("Descrição: " + description);
+
+    // Limpa o formulário após o envio
+    document.getElementById("problemForm").reset();
+});
